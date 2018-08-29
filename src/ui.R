0bb7394595428228cd73b3209033f3105ee92bde
Martin Eden <martin.s.eden@pm.me>
2018-08-29 18:09:16 +0100

    Add digest placeholders

--- src/ui.R
+++ src/ui.R
@@ -2,10 +2,25 @@ ui <- fluidPage(
     includeCSS("css/style.css"),
     includeCSS("css/bootstrap4.css"),
     div(class="row align-items-center",
-        div("Shiny 90", class="header",
+        div("", class="header",
             tags$small("", class="pull-right",
                 a("About this program and the underlying model", href="#")
-            )
+            ),
+            div("", class="pull-left digest-controls",
+                div("", class="row",
+                    a("", href="#", class="col-md-6 control",
+                        img(id="digest-download", src="images/cloud-download.svg", width=32, height=32),
+                        tags$br(),
+                        tags$label(`for`="digest-download", "Save")
+                    ),
+                    a("", href="#", class="col-md-6",
+                        img(id="digest-upload", src="images/cloud-upload.svg", width=32, height=32),
+                        tags$br(),
+                        tags$label(`for`="digest-upload", "Load")
+                    )
+                )
+            ),
+            div("Shiny 90", class="title")
         )
     ),
     div(class="row main-content",
