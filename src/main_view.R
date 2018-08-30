01258736bef88a1f25393df96d92d5d0721d97af
Martin Eden <martin.s.eden@pm.me>
2018-08-30 14:51:00 +0100

    Restore download & upload buttons

--- src/main_view.R
+++ src/main_view.R
@@ -2,14 +2,26 @@ mainView <- function() {
     div("",
         div("", class="header",
             fluidRow(
-                div("", class="col-md-6 col-sm-12 info",
-                    div("",
-                        tags$strong("Current working set:"),
-                        textOutput("workingSet_name", inline=TRUE)
+                div("", class="col-md-6 col-sm-12",
+                    div("", class="info",
+                        div("",
+                            tags$strong("Current working set:"),
+                            textOutput("workingSet_name", inline=TRUE)
+                        ),
+                        div("",
+                            tags$strong("Working set notes:"),
+                            textOutput("workingSet_notes", inline=TRUE)
+                        )
                     ),
-                    div("",
-                        tags$strong("Working set notes:"),
-                        textOutput("workingSet_notes", inline=TRUE)
+                    a("", href="#", class="digest-button",
+                        img(id="digest-download", src="images/cloud-download.svg", width=32, height=32),
+                        tags$br(),
+                        tags$label(`for`="digest-download", "Save")
+                    ),
+                    a("", href="#", class="digest-button",
+                        img(id="digest-upload", src="images/cloud-upload.svg", width=32, height=32),
+                        tags$br(),
+                        tags$label(`for`="digest-upload", "Load")
                     )
                 ),
                 div("", class="col-md-6 col-sm-12 text-right",
