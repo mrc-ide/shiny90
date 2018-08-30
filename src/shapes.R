65cb048c8380827465b8364a008faddbaa1805d8
Martin Eden <martin.s.eden@pm.me>
2018-08-30 18:50:08 +0100

    Show working set in "editable" box

---
+++ src/shapes.R
@@ -0,0 +1,5 @@
+edit_link <- function() {
+    a("Edit", href="#", title="Edit",
+        HTML('<svg xmlns="http://www.w3.org/2000/svg" fill="#337ab7" width="14" height="16" viewBox="0 0 14 16"><path fill-rule="evenodd" d="M0 12v3h3l8-8-3-3-8 8zm3 2H1v-2h1v1h1v1zm10.3-9.3L12 6 9 3l1.3-1.3a.996.996 0 0 1 1.41 0l1.59 1.59c.39.39.39 1.02 0 1.41z"/></svg>')
+    )
+}
