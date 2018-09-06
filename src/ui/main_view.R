b7167d42cfd249a0b744cd02afeb8039d8440edc
Martin Eden <martin.s.eden@pm.me>
2018-09-06 11:26:08 +0100

    Reorganize code

--- src/ui/main_view.R
+++ src/ui/main_view.R
@@ -8,18 +8,7 @@ mainView <- function() {
                     )
                 ),
                 div("",
-                    div("", style="width: 64px; display: inline-block",
-                        a("", href="#", class="digest-button btn-sq btn-sq-sm btn btn-red btn-default text-center",
-                            img(id="digest-download", src="images/cloud-download-red.svg", width=32, height=32),
-                            tags$br(),
-                            tags$label(`for`="digest-download", "Save")
-                        ),
-                        a("", href="#", class="digest-button btn-sq btn-sq-sm btn btn-red btn-default text-center",
-                            img(id="digest-upload", src="images/cloud-upload-red.svg", width=32, height=32),
-                            tags$br(),
-                            tags$label(`for`="digest-upload", "Load")
-                        )
-                    ),
+                    digestButtons(),
                     div("", class="working-set",
                         div("",
                             tags$strong("Current working set:"),
