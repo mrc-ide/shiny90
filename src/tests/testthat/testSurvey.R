7ad472c6c426337ea0664536b8e09164df0e3728
Alex Hill <alex.hill@gmail.com>
2018-10-02 15:23:52 +0100

    failing test

--- src/tests/testthat/testSurvey.R
+++ src/tests/testthat/testSurvey.R
@@ -15,6 +15,24 @@ testthat::test_that("can upload a new set of survey data for the same country", 
 
     Sys.sleep(1)
     rows <- wd$findElements("css", "#hot_survey .ht_master tbody .rowHeader")
-    
+
     testthat::expect_equal(length(rows), 4)
+})
+
+testthat::test_that("cannot upload a csv with wrong headers", {
+    wd$navigate(appURL)
+
+    startNewWorkingSet(wd)
+
+    uploadSpectrumFile(wd, filename= "Malawi_2018_version_8.PJNZ")
+    section <- wd$findElement("css", ".uploadedSpectrumFilesSection")
+    waitForVisible(section)
+
+    switchTab(wd, "Upload survey data")
+    uploadFile(wd, filename = "badsurvey_malawi.csv", inputId="#surveyData")
+
+    Sys.sleep(1)
+    rows <- wd$findElements("css", "#hot_survey .ht_master tbody .rowHeader")
+
+    testthat::expect_equal(length(rows), 0)
 })
