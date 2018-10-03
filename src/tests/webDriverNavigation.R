0c5e5af3b53bf5768fc3e0f0aab151d9fbf5fff6
Alex Hill <alex.hill@gmail.com>
2018-10-03 12:29:46 +0100

    add one more wait

--- src/tests/webDriverNavigation.R
+++ src/tests/webDriverNavigation.R
@@ -1,7 +1,12 @@
 switchTab <- function(wd, tabText) {
     selector <- glue::glue("a[data-toggle=tab][data-value='{tabText}']")
+    link <- wd$findElement("css", selector)
+
+    waitFor(function(){
+        link$getElementAttribute("class") != "disabled"
+    })
+
     waitFor(function() {
-        link <- wd$findElement("css", selector)
         link$clickElement()
         title <- wd$findElement("css", inActivePane(".panelTitle"))
         tabText == getText(title)
