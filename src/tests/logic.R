a325a1d02342d90bbb5b0fad22975f4bfd345733
Alex Hill <alex.hill@gmail.com>
2018-10-03 11:10:36 +0100

    wait for element

--- src/tests/logic.R
+++ src/tests/logic.R
@@ -2,8 +2,10 @@ startNewWorkingSet <- function(wd) {
     enterText(wd$findElement("css", "#workingSetName"), "Selenium working set")
     Sys.sleep(0.5)
     wd$findElement("css", "#startNewWorkingSet")$clickElement()
-    Sys.sleep(0.5)
-    expectTextEqual("Selenium working set", wd$findElement("css", "#workingSet_name"))
+    
+    workingSetName <- wd$findElement("css", "#workingSet_name")
+    waitForVisible(workingSetName)
+    expectTextEqual("Selenium working set", workingSetName)
 }
 
 uploadSpectrumFile <- function(wd, dir = "../../../sample_files/", filename = "Malawi_2018_version_8.PJNZ") {
