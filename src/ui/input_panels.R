b13584cc3dded483ce3ad3bcf8b4daa195ecd5f1
Rob <r.ashton@imperial.ac.uk>
2019-01-23 10:24:51 +0000

    Style download digest button on review input page

--- src/ui/input_panels.R
+++ src/ui/input_panels.R
@@ -71,10 +71,13 @@ panelReviewInput <- function() {
             Please review it, and go back and edit your data if anything doesn't look right.",
             class = "mb-3"
         ),
-        shiny::div("", class="suggest-save",
-            shiny::span("Once you have reviewed your input data, you may want to "),
-            shiny::tags$a(class = "shiny-download-link", href = "", "download a digest file", id = "digestDownload3", download = NA, target = "_blank"),
-            shiny::span("containing your input data and results. You can re-upload this file later to view your results again and change your input data.")
+        shiny::div("", class="suggest-save mb-3",
+            shiny::span("Once you have reviewed your input data, you may want to save your work. This 
+                        downloads a file containing your input data and results. You can re-upload this 
+                        later to view your results again and change your input data.")
+        ),
+        shiny::div("", class="save-button",
+            shiny::downloadButton('digestDownload3', "Save your work")
         ),
         shiny::conditionalPanel(
             condition = "output.incompleteProgramData",
