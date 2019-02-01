92a45c08cbba6e58e9ae37534e5dfc3aed8ce1fb
Alex <alex.hill@gmail.com>
2019-02-01 16:13:54 +0000

    fully scripted building usb

--- scripts/build_usb.sh
+++ scripts/build_usb.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 here=$(dirname "$0")
+usb_dir="usb"
+app_dir="${usb_dir}/app"
 
 $here/compile.R
 
-mkdir usb
+mkdir -p $app_dir
 
-cp .Renviron LICENSE app.R run.R launch.bat usb
-cp images src usb -r
-cp compiled usb/packages -r
+cp .Renviron LICENSE app.R run.R $here/launch.bat ${app_dir}
+cp $here/windows_desktop_readme.md ${usb_dir}/README.md
+cp images src ${app_dir}  -r
+cp compiled/windows ${app_dir}/packages -r
+cp compiled/extra ${usb_dir} -r
---
+++ scripts/windows_desktop_readme.md
@@ -0,0 +1,9 @@
+### Running without RStudio
+
+1. Save this directory to your desktop
+2. Install R by double clicking on the "R-3.5.2-win.exe" file in 
+the "extra" folder in this directory
+3. Double click "launch.bat" in the "app" folder of this directory
+
+Note: if you already have R installed, you must have the same version as 
+is included here
